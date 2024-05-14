8050961572a09c41fe66aff70e295b9304231d51
Yuri-der-Haribon <ajeti.deonis@icloud.com>
2024-05-14 11:25:32 +0200

    Variable og = 10

--- Main.java
+++ Main.java
@@ -3,13 +3,14 @@ public class Main
 {
     public static void main(String args[])
     {
+        int og = 10;
         int summe = 0;
-        for(int i = 1; i <= 10; i++){
+        for(int i = 1; i <= og; i++){
             summe = summe +1;
         }
         
         int multi = 1;
-        for(int i = 1; i <= 10; i++){
+        for(int i = 1; i <= og; i++){
             summe = summe * i;
         }
         
