f619d5fbb18b8ff325ba24cd0852b669859bc730
Yuri-der-Haribon <ajeti.deonis@icloud.com>
2024-05-14 11:01:34 +0200

    Summe implementiert

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ public class Main
 {
     public static void main(String args[])
     {
-        
+        int summe = 0;
+        for(int i = 1; i <= 10; i++){
+            summe += 1;
+        }
     }
 }
