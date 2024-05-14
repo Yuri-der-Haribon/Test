a21feb0ff36e01147c74f395f9a34f78f457be85
Yuri-der-Haribon <ajeti.deonis@icloud.com>
2024-05-14 11:27:01 +0200

    Variable obereGrenze = 50

--- Main.java
+++ Main.java
@@ -3,13 +3,14 @@ public class Main
 {
     public static void main(String args[])
     {
+        int obereGrenze = 50;
         int summe = 0;
-        for(int i = 1; i <= 10; i++){
+        for(int i = 1; i <= obereGrenze; i++){
             summe = summe +1;
         }
         
         int multi = 1;
-        for(int i = 1; i <= 10; i++){
+        for(int i = 1; i <= obereGrenze; i++){
             summe = summe * i;
         }
         
